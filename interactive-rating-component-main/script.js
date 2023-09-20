651dd9be469762a4aa55e996b0853961c4aa8c5a
arianna choza <ariannachoza@ariannas-MacBook-Pro.local>
2023-09-20 17:04:41 -0400

    adding javascript

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -6,3 +6,7 @@
 // have number appear on thank you
 // hide rating
 
+const submit = document.getElementById("submit-button")
+const ratingCard = document.getElementById("rating-card")
+const thankYouCard = document.getElementById("thank-you-card")
+submit.onclick = () => console.log("clicked")
